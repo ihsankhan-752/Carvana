e0ecacd7ac46a62044965e6c9d4f86a284e5f09c
ihsankhan-752 <ihsankhan.se752@gmail.com>
2024-10-25 14:36:02 +0500

    App Architecture created

---
+++ lib/res/colors/app_colors.dart
@@ -0,0 +1,9 @@
+import 'dart:ui';
+
+class AppColors {
+  static const Color bgColor = Color(0xFFF6F6FB);
+  static const Color primaryColor = Color(0xFF3C87C6);
+  static const Color secondaryColor = Color(0xFF6B77C0);
+  static const Color primaryWhite = Color(0xFFFFFFFF);
+  static const Color primaryBlack = Color(0xFF000000);
+}
