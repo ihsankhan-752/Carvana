b2b41758a620f40c605abd8c2c7696ddc88f00b1
ihsankhan-752 <ihsankhan.se752@gmail.com>
2024-10-27 14:53:51 +0500

    Firebase Setup Done

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,17 @@
 import 'package:carvana/res/colors/app_colors.dart';
 import 'package:carvana/res/routes/routes.dart';
 import 'package:carvana/view/splash/splash_view.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
-void main() {
+import 'firebase_options.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
