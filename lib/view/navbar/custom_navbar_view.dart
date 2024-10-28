cb17e67125221b760c8f032c60e05ba5edbd5c90
ihsankhan-752 <ihsankhan.se752@gmail.com>
2024-10-28 14:28:36 +0500

    Auth Section Done

---
+++ lib/view/navbar/custom_navbar_view.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class CustomNavbarView extends StatefulWidget {
+  const CustomNavbarView({super.key});
+
+  @override
+  State<CustomNavbarView> createState() => _CustomNavbarViewState();
+}
+
+class _CustomNavbarViewState extends State<CustomNavbarView> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
