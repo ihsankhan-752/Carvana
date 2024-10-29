d190e731dc8eb56749bb0fac022c87b34bba7ddc
ihsankhan-752 <ihsankhan.se752@gmail.com>
2024-10-29 12:01:58 +0500

    User Session done

--- lib/view_model/services/splash_services.dart
+++ lib/view_model/services/splash_services.dart
@@ -1,12 +1,25 @@
 import 'dart:async';
 
 import 'package:carvana/res/routes/routes_name.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:get/get.dart';
 
 class SplashServices {
   void isUserLogin() {
-    Timer(const Duration(seconds: 4), () {
-      Get.toNamed(RoutesName.firstOnboardingView);
-    });
+    if (FirebaseAuth.instance.currentUser != null) {
+      Timer(
+        const Duration(seconds: 4),
+        () {
+          Get.toNamed(RoutesName.navbarView);
+        },
+      );
+    } else {
+      Timer(
+        const Duration(seconds: 4),
+        () {
+          Get.toNamed(RoutesName.firstOnboardingView);
+        },
+      );
+    }
   }
 }
