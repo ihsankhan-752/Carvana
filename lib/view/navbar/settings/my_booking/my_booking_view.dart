77e205c13ea3a94143f59f6b2e751e05912529a0
ihsankhan-752 <ihsankhan.se752@gmail.com>
2024-11-16 13:04:24 +0500

    Booking View Code Refactor

--- lib/view/navbar/settings/my_booking/my_booking_view.dart
+++ lib/view/navbar/settings/my_booking/my_booking_view.dart
@@ -1,11 +1,12 @@
 import 'package:carvana/models/car/car_model.dart';
 import 'package:carvana/res/colors/app_colors.dart';
 import 'package:carvana/res/text_styles/app_text_styles.dart';
+import 'package:carvana/view/navbar/settings/my_booking/widgets/booking_info_widget.dart';
+import 'package:carvana/view/navbar/settings/my_booking/widgets/car_info_widget.dart';
 import 'package:carvana/view_model/controllers/booking/booking_view_controller.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:intl/intl.dart';
 
 import '../../../../models/booking/booking_model.dart';
 
@@ -60,46 +61,7 @@ class MyBookingView extends StatelessWidget {
                               ),
                             ),
                             const SizedBox(height: 20),
-                            Row(
-                              children: [
-                                Image.network(car.imageUrl, width: Get.width * 0.4, fit: BoxFit.scaleDown),
-                                Container(height: Get.height * 0.15, width: 0.5, color: AppColors.primaryColor),
-                                Padding(
-                                  padding: const EdgeInsets.all(10),
-                                  child: Column(
-                                    crossAxisAlignment: CrossAxisAlignment.start,
-                                    children: [
-                                      RichText(
-                                        text: TextSpan(children: [
-                                          TextSpan(text: "${car.name} ", style: AppTextStyles.h1Bold.copyWith(fontSize: 12)),
-                                          TextSpan(text: "(${car.brand})", style: AppTextStyles.h1.copyWith(fontSize: 12)),
-                                        ]),
-                                      ),
-                                      const SizedBox(height: 5),
-                                      Text(
-                                        "${car.pricePerHour.toStringAsFixed(1)} Per hour",
-                                        style: AppTextStyles.h1Bold.copyWith(fontSize: 12),
-                                      ),
-                                      const SizedBox(height: 5),
-                                      Text(
-                                        car.transmission,
-                                        style: AppTextStyles.h1Bold.copyWith(fontSize: 12),
-                                      ),
-                                      const SizedBox(height: 5),
-                                      Text(
-                                        car.fuelType,
-                                        style: AppTextStyles.h1Bold.copyWith(fontSize: 12),
-                                      ),
-                                      const SizedBox(height: 5),
-                                      Text(
-                                        "A/C :${car.airConditioning}",
-                                        style: AppTextStyles.h1Bold.copyWith(fontSize: 12),
-                                      ),
-                                    ],
-                                  ),
-                                ),
-                              ],
-                            ),
+                            CarInfoWidget(car: car),
                             const Divider(),
                             const SizedBox(height: 10),
                             Center(
@@ -110,30 +72,7 @@ class MyBookingView extends StatelessWidget {
                               ),
                             ),
                             const SizedBox(height: 10),
-                            Text(
-                              "Booking Date : ${DateFormat("MM/dd/yyyy").format(booking.pickUpDate)}",
-                              style: AppTextStyles.h1Bold.copyWith(fontSize: 12),
-                            ),
-                            const SizedBox(height: 5),
-                            Text(
-                              "Pick-Up Time : ${booking.pickUpTime}",
-                              style: AppTextStyles.h1Bold.copyWith(fontSize: 12),
-                            ),
-                            const SizedBox(height: 5),
-                            Text(
-                              "Return Time : ${booking.returnTime}",
-                              style: AppTextStyles.h1Bold.copyWith(fontSize: 12),
-                            ),
-                            const SizedBox(height: 5),
-                            Text(
-                              "Total Price : ${booking.totalPrice.toStringAsFixed(1)} USD",
-                              style: AppTextStyles.h1Bold.copyWith(fontSize: 12),
-                            ),
-                            const SizedBox(height: 5),
-                            Text(
-                              "Booking Status : ${booking.bookingStatus}",
-                              style: AppTextStyles.h1Bold.copyWith(fontSize: 12),
-                            ),
+                            BookingInfoWidget(booking: booking),
                           ],
                         ),
                       ),
